79e58ed7fa4c461ede99f3ecb9c96cf0a8d7cf66
Jacob Petrakovitz <japesc20@gmail.com>
2022-01-16 16:28:58 -0500

    Blog 2 written

--- src/config/data.js
+++ src/config/data.js
@@ -1,17 +1,18 @@
 // Markdown Notes: \n\n = Break, **text** = Bold, *Text* = italic, * Text = li 
 
 const Data = [
- {
-  id: 3,
-  title: 'The Beginning... Again',
-  category: 'diet',
-  subCategory: ['exercise', 'diet', 'health'],
-  description: "This is blog number 3.",
-  authorName: 'Jacob Petrakovitz',
-  authorAvatar: '/assets/images/profile-pic.jpg',
-  createdAt: 'Jan 8, 2022',
-  cover: '/assets/images/beginning.jpg'
-  },
+
+//  {
+//   id: 3,
+//   title: 'The Beginning... Again',
+//   category: 'diet',
+//   subCategory: ['exercise', 'diet', 'health'],
+//   description: "This is blog number 3.",
+//   authorName: 'Jacob Petrakovitz',
+//   authorAvatar: '/assets/images/profile-pic.jpg',
+//   createdAt: 'Jan 8, 2022',
+//   cover: '/assets/images/beginning.jpg'
+//   },
   
   // "The background in fitness and health definitely isn't exciting nor is it short, the road was long and rough among many other things. I began after highschool in 2008, I asked my best friend who was a lifting stud to show me... literally anything. We ended up lifting together for another year and half, with a lot of knowledge gained and eventually to find out a shoulder injury later. I did something very much out of character, I joined the Marine Corps",
 
@@ -20,7 +21,8 @@ const Data = [
     title: 'Background and History',
     category: 'fitness',
     subCategory: ['exercise', 'diet', 'health'],
-    description: "This is blog number 2.",
+    previewDescription: "The background in fitness and health definitely isn't exciting nor is it short, the road was long and rough among many other things.",
+    description: "### The Beginning Again \n\n The background in fitness and health definitely isn't exciting nor is it short, the road was long and rough among many other things. I began after highschool in 2008, I asked my best friend who was a lifting stud to show me... literally anything. \n\n We ended up lifting together for another year and half, with a lot of knowledge gained and eventually to find out a shoulder injury later. I did something very much out of character, I joined the Marine Corps. \n\n With health and fitness becoming a big part of my life, the more I grew to really enjoy it. \n\n Over the next 11 years I would be consistent and inconsistent, but never inconsistent enough to really fall out of working out... until 2020. \n\n The summer of 2020 my shoulder injury from before had finally got to a point that I could'nt perform a single pull up. \n\n I finally decided to take the longest break from lifting I'd ever done. \n\n **6 Months.** \n\n Once I began again, life started to get in the way. \n\n Covid, new job, losing new job, moving across the state. \n\n The more time spent away from the gym, the harder it became to return, at least from my experience. As Newton would say, \n\n *'A body at rest will remain at rest unless an outside force acts on it'* \n\n And that's where we begin, my journey back into health and fitness or just simply as, \n\n The Beginning... Again.",
      authorName: 'Jacob Petrakovitz',
     authorAvatar: '/assets/images/profile-pic.jpg',
     createdAt: 'Jan 8, 2022',
